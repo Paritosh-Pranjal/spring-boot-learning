91fac62db1e59339be28084d21fb9158d5ba983c
Paritosh-Pranjal <paritoshpranjal14@gmail.com>
2025-09-12 02:44:11 +0530

    feat: add spring security

---
+++ spring-security/03-spring-boot-rest-security-jdbc-bcrypt-password/sql-scripts/05-setup-spring-security-demo-database-bcrypt.sql
@@ -0,0 +1,87 @@
+-- Drop database if it already exists
+DROP
+DATABASE IF EXISTS `employee_security_directory_bcrypt`;
+
+-- Create a new database
+CREATE
+DATABASE `employee_security_directory_bcrypt`;
+
+-- Select the new database
+USE `employee_security_directory_bcrypt`;
+
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: https://www.luv2code.com/generate-bcrypt-password
+--
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `users` 
+VALUES 
+('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities4_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+('john','ROLE_EMPLOYEE'),
+('mary','ROLE_EMPLOYEE'),
+('mary','ROLE_MANAGER'),
+('susan','ROLE_EMPLOYEE'),
+('susan','ROLE_MANAGER'),
+('susan','ROLE_ADMIN');
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee`
+(
+    `id`         int NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(45) DEFAULT NULL,
+    `last_name`  varchar(45) DEFAULT NULL,
+    `email`      varchar(45) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Data for table `employee`
+--
+
+INSERT INTO `employee`
+VALUES (1, 'Leslie', 'Andrews', 'leslie@luv2code.com'),
+       (2, 'Emma', 'Baumgarten', 'emma@luv2code.com'),
+       (3, 'Avani', 'Gupta', 'avani@luv2code.com'),
+       (4, 'Yuri', 'Petrov', 'yuri@luv2code.com'),
+       (5, 'Juan', 'Vega', 'juan@luv2code.com');
