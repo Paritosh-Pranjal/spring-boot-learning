47516606f82a333e240a7e04cee661dea0a57650
Paritosh-Pranjal <paritoshpranjal14@gmail.com>
2025-09-15 11:39:36 +0530

    feat: springboot-basic security and mvc in-memory-security

---
+++ 11-springboot-mvc-security/01-springboot-mvc-security-default/src/main/java/com/learn/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.learn.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showLoginPage(){
+        return "fancy-login";
+    }
+
+    // add request mapping for /access-denied
+    @GetMapping("/access-denied")
+    public String showAccessDeniedPage(){
+        return "access-denied";
+    }
+}
